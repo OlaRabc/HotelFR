09a1a3ffa16d171dee3d59c6e9cd6dcf76a5450d
Ola <95347335+OlaRabc@users.noreply.github.com>
2022-04-30 14:02:47 +0200

    addAllFile

---
+++ src/components/const/footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+const Footer = () => {
+
+    return (
+        <div id="foot">
+           <p id="copyright"> Copyright © 2022 MWLzG - All Rights Reserved </p>
+        </div>
+    )
+}
+ 
+export default Footer;
